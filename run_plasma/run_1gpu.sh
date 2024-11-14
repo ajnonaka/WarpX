e4d61500fba8b8a5a2c89320739ce75cf9f71052
Andy Nonaka <AJNonaka@lbl.gov>
2024-11-14 10:56:02 -0800

    scripts, inputs

---
+++ run_plasma/run_1gpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C gpu
+#SBATCH -G 1
+#SBATCH -q debug
+#SBATCH -J WarpX_plasma
+#SBATCH -t 00:05:00
+#SBATCH -A mp111_g
+
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+# pin to closest NIC to GPU
+export MPICH_OFI_NIC_POLICY=GPU
+
+srun -n 1 -c 128 --cpu_bind=cores -G 1 --gpu-bind=none ./warpx.2d.MPI.CUDA.DP.PDP.OPMD.EB inputs_plasma my_constants.scaling_fac=1
