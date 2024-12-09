259d42b854b8ee5d99df46c55aef4b8e8197ed42
Andy Nonaka <AJNonaka@lbl.gov>
2024-12-09 08:48:30 -0800

    fix gpu binding

--- run_plasma/run_32gpu.sh
+++ run_plasma/run_32gpu.sh
@@ -12,7 +12,8 @@ export OMP_NUM_THREADS=1
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
-#run the application:
-#applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
-srun -n 32 -c 32 --cpu_bind=cores -G 32 --gpu-bind=single:1  ./warpx.2d.MPI.CUDA.DP.PDP.OPMD.EB inputs.2d_32gpu
+# pin to closest NIC to GPU
+export MPICH_OFI_NIC_POLICY=GPU
+
+srun -n 32 -c 32 --cpu_bind=cores -G 32 --gpu-bind=none  ./warpx.2d.MPI.CUDA.DP.PDP.OPMD.EB inputs.2d_32gpu
 
--- run_plasma/run_8gpu.sh
+++ run_plasma/run_8gpu.sh
@@ -12,7 +12,8 @@ export OMP_NUM_THREADS=1
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
-#run the application:
-#applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
-srun -n 8 -c 32 --cpu_bind=cores -G 8 --gpu-bind=single:1  ./warpx.2d.MPI.CUDA.DP.PDP.OPMD.EB inputs.2d_8gpu
+# pin to closest NIC to GPU
+export MPICH_OFI_NIC_POLICY=GPU
+
+srun -n 8 -c 32 --cpu_bind=cores -G 8 --gpu-bind=none  ./warpx.2d.MPI.CUDA.DP.PDP.OPMD.EB inputs.2d_8gpu
 
